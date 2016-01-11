6da3644ce90c13a0ef0402494d35e25f7d3fca2b
Santia <msantia09@gmail.com>
2016-01-11 14:17:39 +0800

    Completed Clearance gem usage for signin/signup/logout.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,4 @@
+class UsersController < ApplicationController
+	before_action :require_login
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+    root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -14,6 +16,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  # resources :users, only: [:new]
   # Example resource route with options:
   #   resources :products do
   #     member do
